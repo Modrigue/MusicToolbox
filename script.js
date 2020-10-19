bf8daa17ae6b4f3911043ee569bf298bd1bad050
Modrigue <thealdobaldo@gmail.com>
2020-10-19 21:09:44 +0200

    added intervals display

--- script.js
+++ script.js
@@ -13,6 +13,21 @@ notesDict[9] = "F#";
 notesDict[10] = "G";
 notesDict[11] = "G#";
 
+// interval dictionnary
+var intervalsDict = {};
+intervalsDict[0] = "T";
+intervalsDict[1] = "b2";
+intervalsDict[2] = "2";
+intervalsDict[3] = "b3";
+intervalsDict[4] = "3";
+intervalsDict[5] = "4";
+intervalsDict[6] = "b5";
+intervalsDict[7] = "5";
+intervalsDict[8] = "b6";
+intervalsDict[9] = "6";
+intervalsDict[10] = "b7";
+intervalsDict[11] = "7";
+
 // scales
 const scale_major_nat = [0, 2, 4, 5, 7, 9, 11];
 
@@ -88,15 +103,22 @@ function update()
   var modeValue = parseInt(scaleMode[1]);
   var scaleValues = getModeNotesValues(scale_major_nat, modeValue);
   
-
-  // build scale
+  // build scale notes list
   var notesScaleResult = "";
   var scaleNotesValues = getScaleNotesValues(noteValue, scaleValues);
   scaleNotesValues.forEach(function (noteValue, index)
   {
     newNote = notesDict[noteValue];
-    notesScaleResult = notesScaleResult.concat(newNote.toString() + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+    notesScaleResult = notesScaleResult.concat(newNote.toString() + "&nbsp;&nbsp;&nbsp;&nbsp;");
   });
-
   document.getElementById('scale_result').innerHTML = notesScaleResult;
+
+  // build intervals list
+  var intervalsResult = "";
+  scaleValues.forEach(function (intervalValue, index)
+  {
+    newInterval = intervalsDict[intervalValue];
+    intervalsResult = intervalsResult.concat(newInterval.toString() + "&nbsp;&nbsp;&nbsp;&nbsp");
+  });
+  document.getElementById('interval_result').innerHTML = intervalsResult;
 }
