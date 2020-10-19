b4d2d57c636093bb165c3fbba63b01fcb08c27ea
Modrigue <thealdobaldo@gmail.com>
2020-10-19 20:36:49 +0200

    added natural major modes

--- script.js
+++ script.js
@@ -16,10 +16,54 @@ notesDict[11] = "G#";
 // scales
 const scale_major_nat = [0, 2, 4, 5, 7, 9, 11];
 
+
+//////////////////////////////// MUSIC FUNCTIONS //////////////////////////////
+
+
 function addToNoteValue(noteValue, interval)
 {
   return ((noteValue + interval) % 12);
 }
+
+function getScaleNotesValues(noteValue, scaleValues)
+{
+  var scaleNotesValues = [];
+
+  scaleValues.forEach(function (interval, index)
+  {
+    newNoteValue = addToNoteValue(noteValue, interval);
+    scaleNotesValues.push(newNoteValue);
+  });
+
+  //alert(scaleNotesValues);
+  return scaleNotesValues;
+}
+
+function getModeNotesValues(scaleValues, modeNumber)
+{
+  var modeNotesValues = [];
+
+  var nbNotes = scaleValues.length;
+  for (i = 0; i < nbNotes; i++)
+  {
+    var index = (i + (modeNumber - 1)) % nbNotes;
+    var noteValue = scaleValues[index];
+    //alert(index + " " + noteValue);
+    modeNotesValues.push(noteValue);
+  }
+
+  var firstInterval = scaleValues[modeNumber - 1];
+  for (i = 0; i < nbNotes; i++)
+  {
+    modeNotesValues[i] = (modeNotesValues[i] - firstInterval + 12) % 12;
+  } 
+
+  //alert(modeNotesValues);
+  return modeNotesValues;
+}
+
+//////////////////////////////////// EVENTS ///////////////////////////////////
+
 
 function onNoteChanged()
 {
@@ -33,18 +77,25 @@ function onScaleChanged()
 
 function update()
 {
+  // get selected note
   noteSelected = document.getElementById("note").value;
-  scaleSelected = document.getElementById("scale").value;
-
   var noteValue = parseInt(noteSelected);
+  
+  // get selected scale and mode
+  scaleSelected = document.getElementById("scale").value;
+  var scaleMode = scaleSelected.split(",");
+  var scaleName = scaleMode[0];
+  var modeValue = parseInt(scaleMode[1]);
+  var scaleValues = getModeNotesValues(scale_major_nat, modeValue);
+  
 
   // build scale
   var notesScaleResult = "";
-  scale_major_nat.forEach(function (interval, index)
+  var scaleNotesValues = getScaleNotesValues(noteValue, scaleValues);
+  scaleNotesValues.forEach(function (noteValue, index)
   {
-    newNoteValue = addToNoteValue(noteValue, interval);
-    newNote = notesDict[addToNoteValue(noteValue, interval)];
-    notesScaleResult = notesScaleResult.concat(newNote.toString() + "&nbsp;&nbsp;&nbsp;");
+    newNote = notesDict[noteValue];
+    notesScaleResult = notesScaleResult.concat(newNote.toString() + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
   });
 
   document.getElementById('scale_result').innerHTML = notesScaleResult;
