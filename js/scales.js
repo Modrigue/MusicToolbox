0d8f05f6e5a23c2859bb61923b423900a026c602
Modrigue <thealdobaldo@gmail.com>
2020-10-21 13:05:21 +0200

    added scales minor refactoring

--- js/scales.js
+++ js/scales.js
@@ -2,23 +2,25 @@
 var scaleFamiliesDict = {};
 
 // 7 notes
-scaleFamiliesDict["major_nat"]   = [0, 2, 4, 5, 7, 9, 11];
-scaleFamiliesDict["minor_harm"]  = [0, 2, 3, 5, 7, 8, 11];
-scaleFamiliesDict["minor_mel"]   = [0, 2, 3, 5, 7, 9, 11];
-scaleFamiliesDict["major_harm"]  = [0, 2, 4, 5, 7, 8, 11];
-scaleFamiliesDict["major_2harm"] = [0, 1, 4, 5, 7, 8, 11];
+scaleFamiliesDict["7major_nat"]   = [0, 2, 4, 5, 7, 9, 11];
+scaleFamiliesDict["7minor_harm"]  = [0, 2, 3, 5, 7, 8, 11];
+scaleFamiliesDict["7minor_mel"]   = [0, 2, 3, 5, 7, 9, 11];
+scaleFamiliesDict["7major_harm"]  = [0, 2, 4, 5, 7, 8, 11];
+scaleFamiliesDict["7major_2harm"] = [0, 1, 4, 5, 7, 8, 11];
+scaleFamiliesDict["7major_neap"]  = [0, 1, 3, 5, 7, 9, 11];
+scaleFamiliesDict["7minor_neap"]  = [0, 1, 3, 5, 7, 8, 11];
+scaleFamiliesDict["7verdi_enigm"] = [0, 1, 4, 6, 8, 10, 11];
 
 // 6 notes
-scaleFamiliesDict["blues"]   = [0, 3, 5, 6, 7, 10];
-scaleFamiliesDict["strange"] = [0, 2, 4, 6, 8, 10];
-scaleFamiliesDict["major_hexa"] = [0, 2, 4, 5, 7, 9];
-scaleFamiliesDict["aug"]   = [0, 3, 4, 7, 8, 11];
-scaleFamiliesDict["prom"]  = [0, 2, 4, 6, 9, 10];
-scaleFamiliesDict["trit"]  = [0, 1, 4, 6, 7, 10];
+scaleFamiliesDict["6blues"]      = [0, 3, 5, 6, 7, 10];
+scaleFamiliesDict["6strange"]    = [0, 2, 4, 6, 8, 10];
+scaleFamiliesDict["6major_hexa"] = [0, 2, 4, 5, 7, 9];
+scaleFamiliesDict["6aug"]       = [0, 3, 4, 7, 8, 11];
+scaleFamiliesDict["6prom"]      = [0, 2, 4, 6, 9, 10];
+scaleFamiliesDict["6tritone"]      = [0, 1, 4, 6, 7, 10];
 
 // 5 notes
-scaleFamiliesDict["major_penta"] = [0, 2, 4, 7, 9];
-scaleFamiliesDict["minor_penta"] = [0, 3, 5, 7, 10];
-scaleFamiliesDict["javanese"]    = [0, 4, 5, 7, 11];
-scaleFamiliesDict["jap_iwato"]   = [0, 1, 5, 7, 8];
-scaleFamiliesDict["jap_kumoi"]   = [0, 2, 3, 7, 9];
+scaleFamiliesDict["6major_penta"] = [0, 2, 4, 7, 9];
+scaleFamiliesDict["6javanese"]    = [0, 4, 5, 7, 11];
+scaleFamiliesDict["6jap_iwato"]   = [0, 1, 5, 7, 8];
+scaleFamiliesDict["6jap_kumoi"]   = [0, 2, 3, 7, 9];
