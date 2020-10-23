aeab9ab3e84a27ccfa7411435a851d719c362e25
Modrigue <thealdobaldo@gmail.com>
2020-10-23 23:11:42 +0200

    added mode number in combobox minor correction

--- js/chords.js
+++ js/chords.js
@@ -18,14 +18,14 @@ chords3Dict["6sus4(no5)"]  = [0, 5, 9];
 
 // chords with 4 notes
 var chords4Dict = {};
-chords4Dict["7M"]    = [0, 4, 7, 11];
-chords4Dict["7"]     = [0, 4, 7, 10];
-chords4Dict["m7"]    = [0, 3, 7, 10];
-chords4Dict["m7M"]   = [0, 3, 7, 11];
-chords4Dict["7b5"]   = [0, 4, 6, 10];
-chords4Dict["m7b5"]  = [0, 3, 6, 10];
-chords4Dict["7M#5"]  = [0, 4, 8, 11];
-chords4Dict["6b5"]   = [0, 4, 6, 9];
-chords4Dict["m6M"]   = [0, 3, 7, 9];
-chords4Dict["dim°7"] = [0, 3, 6, 9];
+chords4Dict["7M"]   = [0, 4, 7, 11];
+chords4Dict["7"]    = [0, 4, 7, 10];
+chords4Dict["m7"]   = [0, 3, 7, 10];
+chords4Dict["m7M"]  = [0, 3, 7, 11];
+chords4Dict["7b5"]  = [0, 4, 6, 10];
+chords4Dict["m7b5"] = [0, 3, 6, 10];
+chords4Dict["7M#5"] = [0, 4, 8, 11];
+chords4Dict["6b5"]  = [0, 4, 6, 9];
+chords4Dict["m6M"]  = [0, 3, 7, 9];
+chords4Dict["m°7"]  = [0, 3, 6, 9];
 chords4Dict["sus2add6b5"] = [0, 2, 6, 9];
