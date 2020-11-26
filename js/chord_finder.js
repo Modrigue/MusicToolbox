4007c2d5eca6c860043a8dda523bce9b196a3c60
Modrigue <thealdobaldo@gmail.com>
2020-11-26 20:12:58 +0100

    first version of chord finder algorithm

---
+++ js/chord_finder.js
@@ -0,0 +1,41 @@
+// find chords from notes
+function findChord(notesValues, onlyFirstNoteAsFundamental = false)
+{
+    if (notesValues == null || notesValues.length < 2  || notesValues.length > 5)
+        return [];
+
+    let chordsArray = [];
+    const nbNotesInChord = notesValues.length;
+    let chordsDict = chords2Dict;
+    if (nbNotesInChord == 3)
+        chordsDict = chords3Dict;
+    if (nbNotesInChord == 4)
+        chordsDict = chords4Dict;
+    else if (nbNotesInChord == 5)
+        chordsDict = chords5Dict;
+
+    // switch fundamental and compute intervals
+    for (let i = 0; i < nbNotesInChord; i++)
+    {
+        if (onlyFirstNoteAsFundamental && i > 0)
+            break;
+
+        let fundamental = notesValues[i];
+        let intervalsValues = [];
+        for (let value of notesValues)
+        {
+            const interval = (value - fundamental + 10*12) % 12;
+            intervalsValues.push(interval);
+        }
+        
+        // sort intervals
+        intervalsValues.sort();
+
+        // search corresponding chord in dictionary
+        const chordId = getKeyFromValue(chordsDict, intervalsValues);
+        if (chordId !=  null && chordId !=  "" && chordId !=  "?")
+            chordsArray.push([fundamental, chordId]);
+    }
+
+    return chordsArray;
+}
