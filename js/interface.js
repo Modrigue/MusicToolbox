ccc47b0d8fca36c139cc2e007972d06bfb660495
Modrigue <thealdobaldo@gmail.com>
2020-12-05 16:36:44 +0100

    refactored interface.js in TS

--- js/interface.js
+++ js/interface.js
@@ -1,38 +1,27 @@
+"use strict";
 const pagesArray = ["page_scale_explorer", "page_scale_finder", "page_chord_explorer"];
 let pageSelected = "";
-
-function initLanguage()
-{
-  const defaultLang = parseCultureParameter();
-  const checkboxLanguage = document.getElementById('checkboxLanguage');
-
-  checkboxLanguage.checked = (defaultLang == "fr");
-
-  document.title = getString("title"); // force update
-
-  updateLocales();
+function initLanguage() {
+    const defaultLang = parseCultureParameter();
+    const checkboxLanguage = document.getElementById('checkboxLanguage');
+    checkboxLanguage.checked = (defaultLang == "fr");
+    document.title = getString("title"); // force update
+    updateLocales();
 }
-
 ////////////////////////////////// SELECTORS //////////////////////////////////
-
-function updateSelectors()
-{
+function updateSelectors() {
     // get selected culture
     const lang = getSelectedCulture();
-  
     // update scale explorer selectors
     updateNoteSelector('note', 3, false);
     updateScaleSelector('scale', "7major_nat,1");
-
     // update scale finder selectors
-    for (let i = 1; i <= 8; i++)
-    {
+    for (let i = 1; i <= 8; i++) {
         const id = i.toString();
-        updateNoteSelector(`note_finder${id}`, -1, true);   
-        initChordSelector(`chord_finder${id}`, -1, true);   
+        updateNoteSelector(`note_finder${id}`, -1, true);
+        initChordSelector(`chord_finder${id}`, "-1", true);
     }
-    updateNoteSelector('note_finder_tonic', -1, true); 
-    
+    updateNoteSelector('note_finder_tonic', -1, true);
     // update chord explorer selectors
     updateNoteSelector('note_explorer_chord', 3, false);
     initChordSelector('chord_explorer_chord', "M", false);
@@ -40,256 +29,187 @@ function updateSelectors()
     for (let i = 1; i <= 6; i++)
         updateNoteSelector(`chord_explorer_note${i}`, -1, true);
 }
-
 // get selected text from selector
-function getSelectorText(id)
-{
+function getSelectorText(id) {
     const selector = document.getElementById(id);
     const selectedIndex = selector.selectedIndex;
     return selector.options[selectedIndex].text;
 }
-
-
 //////////////////////////////////// EVENTS ///////////////////////////////////
-
-function selectNoteAndScale(scaleId)
-{
+function selectNoteAndScale(scaleId) {
     const scaleAttributes = scaleId.split("|");
     const tonicValue = parseInt(scaleAttributes[0]);
     const scaleKey = scaleAttributes[1];
-
     const noteSelect = document.getElementById('note');
     const scaleSelect = document.getElementById('scale');
-
     // select note and scale
     noteSelect.selectedIndex = tonicValue;
     scaleSelect.selectedIndex = getSelectorIndexFromValue(scaleSelect, scaleKey);
     update();
 }
-
-function initPagefromURLParams()
-{
+function initPagefromURLParams() {
     // parse URL parameters
     const chordParam = parseParameterById("chord");
     const scaleParam = parseParameterById("scale");
     const chordSpecified = (chordParam != null && chordParam != "");
     const scaleSpecified = (scaleParam != null && scaleParam != "");
-
     let specifiedPage = "";
     if (scaleSpecified)
         specifiedPage = "page_scale_explorer";
     else if (chordSpecified)
         specifiedPage = "page_chord_explorer";
-
     selectPage(specifiedPage);
 }
-
-function selectPage(pageId = "")
-{
-   for (let id of pagesArray)
-    {
+function selectPage(pageId = "") {
+    for (let id of pagesArray) {
         let button = document.getElementById(`button_${id}`);
         const buttonSelected = (id == pageId);
-        button.className = buttonSelected ? "button-page-selected" :  "button-page";
+        button.className = buttonSelected ? "button-page-selected" : "button-page";
     }
     pageSelected = pageId;
-
     update();
 }
-
-function onNoteChanged()
-{
+function onNoteChanged() {
     update();
 }
-
-function onScaleChanged()
-{
+function onScaleChanged() {
     update();
 }
-
 // compute and update results
-function update()
-{
+function update() {
     // display selected page
     for (let pageId of pagesArray)
         setVisible(pageId, (pageId == pageSelected));
-
     // get selected note and scale/mode values
     const noteValue = getSelectedNoteValue();
     const scaleValues = getSelectedScaleValues();
     const charIntervals = getSelectedScaleCharIntervals();
-
     const nbNotesInScale = scaleValues.length;
-    
     // build scale notes list
     const scaleNotesValues = getScaleNotesValues(noteValue, scaleValues);
     document.getElementById('scale_result').innerHTML = getScaleNotesTableHTML(noteValue, scaleValues, charIntervals);
-
     // build chords 3,4 notes harmonization tables
     const showChords3 = (nbNotesInScale >= 6);
     const showChords4 = (nbNotesInScale >= 7);
     document.getElementById('chords3_result').innerHTML = showChords3 ? getChordsTableHTML(scaleValues, scaleNotesValues, 3) : "";
     document.getElementById('chords4_result').innerHTML = showChords4 ? getChordsTableHTML(scaleValues, scaleNotesValues, 4) : "";
-
     const scaleName = getSelectorText("scale");
-
     // checkboxes
     //setEnabled("checkboxChords3", showChords3);
     //setEnabled("checkboxChords4", showChords4);
     setEnabled("checkboxChords", showChords3);
-
     // update fretboard
     updateFretboard(noteValue, scaleValues, charIntervals, scaleName);
     updateFretboard(noteValue, scaleValues, charIntervals, scaleName); // HACK to ensure correct drawing
-
     // update keyboard
     updateKeyboard(noteValue, scaleValues, charIntervals, scaleName);
     updateKeyboard(noteValue, scaleValues, charIntervals, scaleName); // HACK to ensure correct drawing
-
     // update scale finder chords selectors
     let has1NoteSelected = false;
-    for (let i = 1; i <= 8; i++)
-    {
+    for (let i = 1; i <= 8; i++) {
         const id = i.toString();
-
         const noteSelected = document.getElementById(`note_finder${id}`).value;
         const noteValue = parseInt(noteSelected);
         const hasNoteSelected = (noteValue >= 0);
-
         if (!hasNoteSelected)
             document.getElementById(`chord_finder${id}`).selectedIndex = 0;
         else
             has1NoteSelected = true;
-
         setEnabled(`chord_finder${id}`, hasNoteSelected);
     }
     setEnabled('reset_scale_finder', has1NoteSelected);
     setEnabled('note_finder_tonic', has1NoteSelected);
     if (!has1NoteSelected)
         document.getElementById("note_finder_tonic").selectedIndex = 0;
-
     // hide welcome page
     if (pageSelected != null && pageSelected != "")
         setVisible('page_welcome', false);
-
     // update found scales given selected page
     const foundScales = document.getElementById('found_scales');
     const foundChordsFromScale = document.getElementById('section_found_chords_from_scale');
-    switch (pageSelected) 
-    {
+    switch (pageSelected) {
         case "page_scale_explorer":
             foundScales.innerHTML = getRelativeScalesHTML(noteValue, scaleValues);
             foundChordsFromScale.innerHTML = findChordsFromScaleScalesHTML(noteValue, scaleValues);
             setVisible('found_scales', true);
             setVisible("section_found_chords_from_scale", true);
             break;
-
         case "page_scale_finder":
             foundScales.innerHTML = findScalesFromNotesHTML();
             setVisible('found_scales', true);
             setVisible("section_found_chords_from_scale", false);
             break;
-
         case "page_chord_explorer":
-        {
-            const checkboxBarres = document.getElementById("checkboxBarres");
-            
-            updateChordGeneratorMode();
-            updateNbStringsSelector();
-            updateFoundChordElements();
-            updateGeneratedChordsOnFretboard(checkboxBarres.checked);
-
-            setVisible('found_scales', false);
-            setVisible("section_found_chords_from_scale", false);
-            break;
-        }
+            {
+                const checkboxBarres = document.getElementById("checkboxBarres");
+                updateChordGeneratorMode();
+                updateNbStringsSelector();
+                updateFoundChordElements();
+                updateGeneratedChordsOnFretboard(checkboxBarres.checked);
+                setVisible('found_scales', false);
+                setVisible("section_found_chords_from_scale", false);
+                break;
+            }
     }
 }
-
-function onResize()
-{
+function onResize() {
     let canvasGuitar = document.getElementById("canvas_guitar");
     canvasGuitar.width = window.innerWidth - 30;
-
     let canvasKeyboard = document.getElementById("canvas_keyboard");
     canvasKeyboard.width = window.innerWidth - 30;
-
     onNoteChanged();
 }
-
-function toggleDisplay(id)
-{
-    let x = document.getElementById(id);
-    
-    if (x.style.display === "none")
-        x.style.display = "block";
+function toggleDisplay(id) {
+    let elem = document.getElementById(id);
+    if (elem.style.display === "none")
+        elem.style.display = "block";
     else
-        x.style.display = "none";
+        elem.style.display = "none";
 }
-
-function setVisible(id, status)
-{
-    let x = document.getElementById(id);
-    x.style.display = status ? "block" : "none";
+function setVisible(id, status) {
+    let elem = document.getElementById(id);
+    elem.style.display = status ? "block" : "none";
 }
-
-function setEnabled(id, status)
-{
-    let x = document.getElementById(id);
-    x.disabled = !status;
+function setEnabled(id, status) {
+    let elem = document.getElementById(id);
+    elem.disabled = !status;
 }
-
-function updateChordGeneratorMode()
-{
+function updateChordGeneratorMode() {
     // get selected mode
     let selectedMode = getSelectedChordGeneratorMode();
     const nameMode = (selectedMode == "name");
-    
     // name mode
     setEnabled("note_explorer_chord", nameMode);
     setEnabled("chord_explorer_chord", nameMode);
     setEnabled("chord_explorer_arpeggio_notes", nameMode);
     setEnabled("chord_explorer_arpeggio_intervals", nameMode);
     setVisible("chord_explorer_arpeggio_texts", nameMode);
-
     // notes mode
     setVisible("chord_explorer_found_chords_texts", !nameMode);
     for (let i = 1; i <= 6; i++)
         setEnabled(`chord_explorer_note${i}`, !nameMode);
 }
-
-function resetScaleFinder()
-{
+function resetScaleFinder() {
     // reset scale finder note selectors
-    for (let i = 1; i <= 8; i++)
-    {
+    for (let i = 1; i <= 8; i++) {
         const id = i.toString();
         let noteSelect = document.getElementById(`note_finder${id}`);
         noteSelect.selectedIndex = 0;
     }
-
     let noteSelect = document.getElementById('note_finder_tonic');
     noteSelect.selectedIndex = 0;
-
     update();
 }
-
 //////////////////////////////////// LOCALES //////////////////////////////////
-
-
-function updateLocales()
-{
+function updateLocales() {
     document.title = getString("title");
-
     // pages buttons
     document.getElementById("button_page_chord_explorer").innerText = getString("page_chord_explorer");
     document.getElementById("button_page_scale_explorer").innerText = getString("page_scale_explorer");
     document.getElementById("button_page_scale_finder").innerText = getString("page_scale_finder");
-
     // welcome
     document.getElementById("welcome_title").innerText = getString("welcome_title");
     document.getElementById("welcome_subtitle").innerText = getString("welcome_subtitle");
-
     // scale explorer
     document.getElementById("select_key_text").innerText = getString("select_key");
     document.getElementById("header_scale_finder").innerText = getString("header_scale_finder");
@@ -297,40 +217,31 @@ function updateLocales()
     document.getElementById("checkboxGuitarLabel").innerText = getString("guitar");
     document.getElementById("checkboxKeyboardLabel").innerText = getString("keyboard");
     document.getElementById("checkboxBarresLabel").innerText = getString("show_barres");
-
     // scale finder
-
     let resetElements = document.getElementsByClassName("reset");
     for (let resetEelem of resetElements)
         resetEelem.innerText = getString("reset");
-
     let tonicElements = document.getElementsByClassName("tonic");
     for (let tonicEelem of tonicElements)
         tonicEelem.innerText = getString("tonic");
-
     // chord explorer
     document.getElementById("radioChordExplorerNameLabel").innerText = getString("name");
     document.getElementById("radioChordExplorerNotesLabel").innerText = getString("notes");
     document.getElementById("play_found_chord").innerText = `${getString("play")} ♪`;
     document.getElementById("play_found_arpeggio").innerText = `${getString("play_arpeggio")} ♪`;
     document.getElementById("chord_explorer_nb_strings_text").innerText = getString("chord_explorer_nb_strings_text");
-    
     // update computed data
     updateSelectors();
     onNoteChanged();
 }
-
-function getSelectorIndexFromValue(selector, value)
-{
+function getSelectorIndexFromValue(selector, value) {
     const options = selector.options;
     const nbOptions = options.length;
-
     let index = -1;
-    for(index = 0; index < nbOptions; index++)
-    {
-        if(options[index].value === value)
+    for (index = 0; index < nbOptions; index++) {
+        if (options[index].value === value)
             return index;
     }
-
     return index;
 }
+//# sourceMappingURL=interface.js.map
---
+++ ts/interface.ts
@@ -0,0 +1,336 @@
+const pagesArray: Array<string> = ["page_scale_explorer", "page_scale_finder", "page_chord_explorer"];
+let pageSelected: string = "";
+
+function initLanguage(): void
+{
+  const defaultLang: string = <string>parseCultureParameter();
+  const checkboxLanguage: HTMLInputElement = <HTMLInputElement>document.getElementById('checkboxLanguage');
+
+  checkboxLanguage.checked = (defaultLang == "fr");
+
+  document.title = getString("title"); // force update
+
+  updateLocales();
+}
+
+////////////////////////////////// SELECTORS //////////////////////////////////
+
+function updateSelectors(): void
+{
+    // get selected culture
+    const lang: string = getSelectedCulture();
+  
+    // update scale explorer selectors
+    updateNoteSelector('note', 3, false);
+    updateScaleSelector('scale', "7major_nat,1");
+
+    // update scale finder selectors
+    for (let i = 1; i <= 8; i++)
+    {
+        const id: string = i.toString();
+        updateNoteSelector(`note_finder${id}`, -1, true);   
+        initChordSelector(`chord_finder${id}`, "-1", true);   
+    }
+    updateNoteSelector('note_finder_tonic', -1, true); 
+    
+    // update chord explorer selectors
+    updateNoteSelector('note_explorer_chord', 3, false);
+    initChordSelector('chord_explorer_chord', "M", false);
+    updateNbStringsSelector();
+    for (let i = 1; i <= 6; i++)
+        updateNoteSelector(`chord_explorer_note${i}`, -1, true);
+}
+
+// get selected text from selector
+function getSelectorText(id: string): string
+{
+    const selector: HTMLSelectElement = <HTMLSelectElement>document.getElementById(id);
+    const selectedIndex: number = selector.selectedIndex;
+    return selector.options[selectedIndex].text;
+}
+
+
+//////////////////////////////////// EVENTS ///////////////////////////////////
+
+function selectNoteAndScale(scaleId: string): void
+{
+    const scaleAttributes: Array<string> = scaleId.split("|");
+    const tonicValue: number = parseInt(scaleAttributes[0]);
+    const scaleKey: string = scaleAttributes[1];
+
+    const noteSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById('note');
+    const scaleSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById('scale');
+
+    // select note and scale
+    noteSelect.selectedIndex = tonicValue;
+    scaleSelect.selectedIndex = getSelectorIndexFromValue(scaleSelect, scaleKey);
+    update();
+}
+
+function initPagefromURLParams(): void
+{
+    // parse URL parameters
+    const chordParam: string = parseParameterById("chord");
+    const scaleParam: string = parseParameterById("scale");
+    const chordSpecified: boolean = (chordParam != null && chordParam != "");
+    const scaleSpecified: boolean = (scaleParam != null && scaleParam != "");
+
+    let specifiedPage: string = "";
+    if (scaleSpecified)
+        specifiedPage = "page_scale_explorer";
+    else if (chordSpecified)
+        specifiedPage = "page_chord_explorer";
+
+    selectPage(specifiedPage);
+}
+
+function selectPage(pageId: string = ""): void
+{
+   for (let id of pagesArray)
+    {
+        let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById(`button_${id}`);
+        const buttonSelected: boolean = (id == pageId);
+        button.className = buttonSelected ? "button-page-selected" :  "button-page";
+    }
+    pageSelected = pageId;
+
+    update();
+}
+
+function onNoteChanged(): void
+{
+    update();
+}
+
+function onScaleChanged(): void
+{
+    update();
+}
+
+// compute and update results
+function update(): void
+{
+    // display selected page
+    for (let pageId of pagesArray)
+        setVisible(pageId, (pageId == pageSelected));
+
+    // get selected note and scale/mode values
+    const noteValue: number = getSelectedNoteValue();
+    const scaleValues: Array<number> = getSelectedScaleValues();
+    const charIntervals: Array<number> = getSelectedScaleCharIntervals();
+
+    const nbNotesInScale: number = scaleValues.length;
+    
+    // build scale notes list
+    const scaleNotesValues: Array<number> = getScaleNotesValues(noteValue, scaleValues);
+    (<HTMLParagraphElement>document.getElementById('scale_result')).innerHTML = getScaleNotesTableHTML(noteValue, scaleValues, charIntervals);
+
+    // build chords 3,4 notes harmonization tables
+    const showChords3 = (nbNotesInScale >= 6);
+    const showChords4 = (nbNotesInScale >= 7);
+    (<HTMLParagraphElement>document.getElementById('chords3_result')).innerHTML = showChords3 ? getChordsTableHTML(scaleValues, scaleNotesValues, 3) : "";
+    (<HTMLParagraphElement>document.getElementById('chords4_result')).innerHTML = showChords4 ? getChordsTableHTML(scaleValues, scaleNotesValues, 4) : "";
+
+    const scaleName: string = getSelectorText("scale");
+
+    // checkboxes
+    //setEnabled("checkboxChords3", showChords3);
+    //setEnabled("checkboxChords4", showChords4);
+    setEnabled("checkboxChords", showChords3);
+
+    // update fretboard
+    updateFretboard(noteValue, scaleValues, charIntervals, scaleName);
+    updateFretboard(noteValue, scaleValues, charIntervals, scaleName); // HACK to ensure correct drawing
+
+    // update keyboard
+    updateKeyboard(noteValue, scaleValues, charIntervals, scaleName);
+    updateKeyboard(noteValue, scaleValues, charIntervals, scaleName); // HACK to ensure correct drawing
+
+    // update scale finder chords selectors
+    let has1NoteSelected: boolean = false;
+    for (let i = 1; i <= 8; i++)
+    {
+        const id = i.toString();
+
+        const noteSelected: string = (<HTMLSelectElement>document.getElementById(`note_finder${id}`)).value;
+        const noteValue: number = parseInt(noteSelected);
+        const hasNoteSelected: boolean = (noteValue >= 0);
+
+        if (!hasNoteSelected)
+            (<HTMLSelectElement>document.getElementById(`chord_finder${id}`)).selectedIndex = 0;
+        else
+            has1NoteSelected = true;
+
+        setEnabled(`chord_finder${id}`, hasNoteSelected);
+    }
+    setEnabled('reset_scale_finder', has1NoteSelected);
+    setEnabled('note_finder_tonic', has1NoteSelected);
+    if (!has1NoteSelected)
+        (<HTMLSelectElement>document.getElementById("note_finder_tonic")).selectedIndex = 0;
+
+    // hide welcome page
+    if (pageSelected != null && pageSelected != "")
+        setVisible('page_welcome', false);
+
+    // update found scales given selected page
+    const foundScales: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById('found_scales');
+    const foundChordsFromScale: HTMLDivElement = <HTMLDivElement>document.getElementById('section_found_chords_from_scale');
+    switch (pageSelected) 
+    {
+        case "page_scale_explorer":
+            foundScales.innerHTML = getRelativeScalesHTML(noteValue, scaleValues);
+            foundChordsFromScale.innerHTML = findChordsFromScaleScalesHTML(noteValue, scaleValues);
+            setVisible('found_scales', true);
+            setVisible("section_found_chords_from_scale", true);
+            break;
+
+        case "page_scale_finder":
+            foundScales.innerHTML = findScalesFromNotesHTML();
+            setVisible('found_scales', true);
+            setVisible("section_found_chords_from_scale", false);
+            break;
+
+        case "page_chord_explorer":
+        {
+            const checkboxBarres: HTMLInputElement = <HTMLInputElement>document.getElementById("checkboxBarres");
+            
+            updateChordGeneratorMode();
+            updateNbStringsSelector();
+            updateFoundChordElements();
+            updateGeneratedChordsOnFretboard(checkboxBarres.checked);
+
+            setVisible('found_scales', false);
+            setVisible("section_found_chords_from_scale", false);
+            break;
+        }
+    }
+}
+
+function onResize(): void
+{
+    let canvasGuitar: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("canvas_guitar");
+    canvasGuitar.width = window.innerWidth - 30;
+
+    let canvasKeyboard: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("canvas_keyboard");
+    canvasKeyboard.width = window.innerWidth - 30;
+
+    onNoteChanged();
+}
+
+function toggleDisplay(id: string): void
+{
+    let elem: HTMLElement = <HTMLElement>document.getElementById(id);
+    
+    if (elem.style.display === "none")
+        elem.style.display = "block";
+    else
+        elem.style.display = "none";
+}
+
+function setVisible(id: string, status: boolean): void
+{
+    let elem: HTMLElement = <HTMLElement>document.getElementById(id);
+    elem.style.display = status ? "block" : "none";
+}
+
+function setEnabled(id: string, status: boolean): void
+{
+    let elem: any = document.getElementById(id);
+    elem.disabled = !status;
+}
+
+function updateChordGeneratorMode(): void
+{
+    // get selected mode
+    let selectedMode: string = getSelectedChordGeneratorMode();
+    const nameMode: boolean = (selectedMode == "name");
+    
+    // name mode
+    setEnabled("note_explorer_chord", nameMode);
+    setEnabled("chord_explorer_chord", nameMode);
+    setEnabled("chord_explorer_arpeggio_notes", nameMode);
+    setEnabled("chord_explorer_arpeggio_intervals", nameMode);
+    setVisible("chord_explorer_arpeggio_texts", nameMode);
+
+    // notes mode
+    setVisible("chord_explorer_found_chords_texts", !nameMode);
+    for (let i = 1; i <= 6; i++)
+        setEnabled(`chord_explorer_note${i}`, !nameMode);
+}
+
+function resetScaleFinder(): void
+{
+    // reset scale finder note selectors
+    for (let i = 1; i <= 8; i++)
+    {
+        const id = i.toString();
+        let noteSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById(`note_finder${id}`);
+        noteSelect.selectedIndex = 0;
+    }
+
+    let noteSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById('note_finder_tonic');
+    noteSelect.selectedIndex = 0;
+
+    update();
+}
+
+//////////////////////////////////// LOCALES //////////////////////////////////
+
+
+function updateLocales(): void
+{
+    document.title = getString("title");
+
+    // pages buttons
+    (<HTMLButtonElement>document.getElementById("button_page_chord_explorer")).innerText = getString("page_chord_explorer");
+    (<HTMLButtonElement>document.getElementById("button_page_scale_explorer")).innerText = getString("page_scale_explorer");
+    (<HTMLButtonElement>document.getElementById("button_page_scale_finder")).innerText = getString("page_scale_finder");
+
+    // welcome
+    (<HTMLHeadElement>document.getElementById("welcome_title")).innerText = getString("welcome_title");
+    (<HTMLHeadElement>document.getElementById("welcome_subtitle")).innerText = getString("welcome_subtitle");
+
+    // scale explorer
+    (<HTMLSpanElement>document.getElementById("select_key_text")).innerText = getString("select_key");
+    (<HTMLSpanElement>document.getElementById("header_scale_finder")).innerText = getString("header_scale_finder");
+    (<HTMLLabelElement>document.getElementById("checkboxChordsLabel")).innerText = getString("chords");
+    (<HTMLLabelElement>document.getElementById("checkboxGuitarLabel")).innerText = getString("guitar");
+    (<HTMLLabelElement>document.getElementById("checkboxKeyboardLabel")).innerText = getString("keyboard");
+    (<HTMLLabelElement>document.getElementById("checkboxBarresLabel")).innerText = getString("show_barres");
+
+    // scale finder
+
+    let resetElements: HTMLCollectionOf<HTMLElement> = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName("reset");
+    for (let resetEelem of resetElements)
+        resetEelem.innerText = getString("reset");
+
+    let tonicElements: HTMLCollectionOf<HTMLElement> = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName("tonic");
+    for (let tonicEelem of tonicElements)
+        tonicEelem.innerText = getString("tonic");
+
+    // chord explorer
+    (<HTMLLabelElement>document.getElementById("radioChordExplorerNameLabel")).innerText = getString("name");
+    (<HTMLLabelElement>document.getElementById("radioChordExplorerNotesLabel")).innerText = getString("notes");
+    (<HTMLButtonElement>document.getElementById("play_found_chord")).innerText = `${getString("play")} ♪`;
+    (<HTMLButtonElement>document.getElementById("play_found_arpeggio")).innerText = `${getString("play_arpeggio")} ♪`;
+    (<HTMLSpanElement>document.getElementById("chord_explorer_nb_strings_text")).innerText = getString("chord_explorer_nb_strings_text");
+    
+    // update computed data
+    updateSelectors();
+    onNoteChanged();
+}
+
+function getSelectorIndexFromValue(selector: HTMLSelectElement, value: string): number
+{
+    const options: HTMLOptionsCollection = selector.options;
+    const nbOptions: number = options.length;
+
+    let index: number = -1;
+    for(index = 0; index < nbOptions; index++)
+    {
+        if(options[index].value === value)
+            return index;
+    }
+
+    return index;
+}
