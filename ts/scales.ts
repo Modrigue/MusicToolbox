b06182fedefe321d075a2e85878ae85df37b51c9
Modrigue <thealdobaldo@gmail.com>
2021-04-26 16:58:10 +0200

    fixed scales

--- ts/scales.ts
+++ ts/scales.ts
@@ -45,11 +45,8 @@ scaleFamiliesDict.set("5phryg6_penta", [0, 1, 3, 7, 9]);
 // 8 notes with quarter tones
 scaleFamiliesDict.set("8bastanikar",   [0, 1.5, 3.5, 5, 6.5, 7.5, 10.5, 11.5]);
 scaleFamiliesDict.set("8dalanshin",    [0, 1, 2, 3.5, 5, 7, 9, 10.5]);
-scaleFamiliesDict.set("8huzam",        [0, 1.5, 3.5, 4.5, 7.5, 8.5, 10.5, 11.5]);
 scaleFamiliesDict.set("8jiharkah",     [0, 2, 4, 5, 7, 9, 10, 10.5]);
-scaleFamiliesDict.set("8mahur",        [0, 2, 3.5, 5, 7, 9, 10, 11]);
 scaleFamiliesDict.set("8mehayar",      [0, 1.5, 3, 5, 7, 8, 8.5, 10]);
-scaleFamiliesDict.set("8mustaar",      [0, 1.5, 2.5, 3.5, 5.5, 6.5, 8.5, 10.5]);
 scaleFamiliesDict.set("8tet",          [0, 1.5, 3, 4.5, 6, 7.5, 9, 10.5]);
 
 // 7 notes with quarter tones
@@ -57,7 +54,10 @@ scaleFamiliesDict.set("7hijaz",        [0, 1, 4, 5, 7, 8.5, 10]);
 scaleFamiliesDict.set("7rast",         [0, 2, 3.5, 5, 7, 9, 10.5]);
 scaleFamiliesDict.set("7bayati",       [0, 1.5, 3, 5, 7, 8, 10]);
 scaleFamiliesDict.set("7hardino",      [0, 1.5, 4, 5, 7, 8.5, 11]);
+scaleFamiliesDict.set("7mahur",        [0, 2, 3.5, 5, 7, 9, 11]);
+scaleFamiliesDict.set("7mustaar",      [0, 2.5, 3.5, 5.5, 6.5, 8.5, 10.5]);
 scaleFamiliesDict.set("7sikah_baladi", [0, 1.5, 3.5, 5, 7, 8.5, 10.5]);
+
 
 // 5 notes with quarter tones
 scaleFamiliesDict.set("5tet_approx",   [0, 2.5, 5, 7, 9.5]);
@@ -185,15 +185,12 @@ scalesDict_int.set("5others,sep", "");
 scalesDict_int.set("8notes_quarter_tones", "--------------- 8 NOTES ¼ TONES ---------------");
 
 scalesDict_int.set("8jiharkah,1,diff:8bebop_dom;1", "Jiharkah");
-scalesDict_int.set("8jiharkah,2,diff:7major_nat;6", "Ushaq Misri (2nd mode)");
+scalesDict_int.set("8jiharkah,2,diff:7major_nat;6", "Ushaq Masri (2nd mode)");
 scalesDict_int.set("8jiharkah,sep", "");
 
 scalesDict_int.set("8bastanikar,1,diff:8bebop_dom;1", "Bastanikar");
 scalesDict_int.set("8dalanshin,1", "Dalanshin");
-scalesDict_int.set("8huzam,1", "Huzam");
-scalesDict_int.set("8mahur,1,diff:8bebop_dom;1", "Mahur");
 scalesDict_int.set("8mehayar,1,diff:7major_nat;6", "Mehayar / Hoseni / Rahaw");
-scalesDict_int.set("8mustaar,1", "Mustaar");
 scalesDict_int.set("8notes_quarter_tones,sep", "");
 
 scalesDict_int.set("8tet,1", "8-TET / 8-EDO");
@@ -204,6 +201,7 @@ scalesDict_int.set("7notes_quarter_tones", "--------------- 7 NOTES ¼ TONES ---
 scalesDict_int.set("7hijaz,1,diff:7minor_harm;5", "Hijaz");
 scalesDict_int.set("7hijaz,4,diff:7major_harm;1", "Suznak (4th mode)");
 scalesDict_int.set("7hijaz,5,diff:7bayati;1", "Bayati Shuri (5th mode)");
+scalesDict_int.set("7hijaz,6", "Huzam (6th mode)");
 scalesDict_int.set("7hijaz,sep", "");
 
 scalesDict_int.set("7rast,1,diff:7major_nat;2", "Rast");
@@ -213,7 +211,12 @@ scalesDict_int.set("7rast,7,diff:7major_nat;1", "Iraq (7th mode)");
 scalesDict_int.set("7rast,sep", "");
 
 scalesDict_int.set("7bayati,1,diff:7major_nat;6", "Bayati");
+scalesDict_int.set("7bayati,4,diff:7major_nat;6", "Ushaq Masri (2nd mode)");
+scalesDict_int.set("7bayati,sep", "");
+
 scalesDict_int.set("7hardino,1,diff:7major_nat;1", "Hardino");
+scalesDict_int.set("7mahur,1,diff:7major_nat;1", "Mahur");
+scalesDict_int.set("7mustaar,1", "Musta'ar");
 scalesDict_int.set("7sikah_baladi,1,diff:7major_nat;1", "Sikah baladi / 7-TET (Approximation)");
 scalesDict_int.set("7notes_quarter_tones,sep", "");
 
@@ -332,15 +335,19 @@ scalesDict_fr.set("5phryg6_penta,1,diff:5major_penta;1", "Pentatonique Phrygien 
 
 scalesDict_fr.set("8notes_quarter_tones", "----------------- 8 NOTES ¼ TONS -----------------");
 
-scalesDict_fr.set("8jiharkah,2,diff:7major_nat;6", "Ushaq Misri (2e mode)");
+scalesDict_fr.set("8jiharkah,2,diff:7major_nat;6", "Ushaq Masri (2e mode)");
 
 scalesDict_fr.set("7notes_quarter_tones", "----------------- 7 NOTES ¼ TONS -----------------");
 
 scalesDict_fr.set("7hijaz,4,diff:7major_harm;1", "Suznak (4e mode)");
 scalesDict_fr.set("7hijaz,5,diff:7bayati;1", "Bayati Shuri (5e mode)");
+scalesDict_fr.set("7hijaz,6", "Huzam (6e mode)");
+
 scalesDict_fr.set("7rast,5,diff:7major_nat;6", "Simdi Huseyni-Ussak (5e mode)");
 scalesDict_fr.set("7rast,6,diff:7major_nat;3", "Ashiran / Arazbar (6e mode)");
 scalesDict_fr.set("7rast,7,diff:7major_nat;1", "Iraq (7e mode)");
+
+scalesDict_int.set("7bayati,4,diff:7major_nat;6", "Ushaq Masri (2e mode)");
 
 scalesDict_fr.set("7sikah_baladi,6,diff:7major_nat;1", "Neutre");
 
@@ -358,7 +365,7 @@ scalesDicts.set("fr",  scalesDict_fr);
 const scalesToHighlight: Array<string> = ["7major_nat,1", "7major_nat,6", "7minor_harm,1", "7minor_melo,1",
     "7major_2harm,1", "7major_harm,1", "7major_neap,1", "7minor_neap,1", "7persian,1",
     "8jiharkah,1",
-    "7bayati,1", "7hardino,1", "7hijaz,1", "7rast,1", "7sikah_baladi",
+    "7bayati,1", "7hardino,1", "7hijaz,1", "7rast,1", "7mahur", "7mustaar", "7sikah_baladi",
     "8bebop_dom,1", "8bebop_maj,1", "8dim,1",
     "6blues,1", "6strange,1", "6aug,1",
     "5major_penta,1", "5major_penta,5", "5jap_in,1", "5jap_insen,1", "5javanese,1",
