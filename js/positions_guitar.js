eb4da75d4387602844d6c24ac529f3ab5c258bc0
Modrigue <thealdobaldo@gmail.com>
2021-06-13 12:36:33 +0200

    refactored scale positions on guitar functions

---
+++ js/positions_guitar.js
@@ -0,0 +1,35 @@
+"use strict";
+//////////////////////////////// GUITAR POSITIONS /////////////////////////////
+function initGuitarPositionSelector(id, useURLParams = true, nbPos = 7, position = -1) {
+    // get selector
+    const posSelect = document.getElementById(id);
+    const initialized = (posSelect.options != null && posSelect.options.length > 0);
+    if (initialized) // nop if already initialized
+        return;
+    // add tunings
+    for (let i = -1; i < nbPos; i++) {
+        const positionAll = (i == -1);
+        let option = document.createElement('option');
+        option.value = i.toString();
+        option.innerHTML = positionAll ? "All" : (i + 1).toString();
+        if (i == position)
+            option.selected = true;
+        posSelect.appendChild(option);
+    }
+    // disable if only 1 option
+    posSelect.disabled = (posSelect.options.length <= 1);
+}
+function updateGuitarPositionGivenNbNotes(id, nbNotes) {
+    const posSelect = document.getElementById(id);
+    const posFormer = getSelectedGuitarPosition(id);
+    // replace selector options and try to select former corresponding guitar tuning
+    removeAllChildNodes(posSelect);
+    const position = (posFormer <= nbNotes) ? posFormer : -1;
+    initGuitarPositionSelector(id, false, nbNotes, position);
+}
+function getSelectedGuitarPosition(id) {
+    const posSelect = document.getElementById(id);
+    let posId = posSelect.value;
+    return parseInt(posId);
+}
+//# sourceMappingURL=positions_guitar.js.map
--- ts/tunings_guitar.ts
+++ ts/tunings_guitar.ts
@@ -117,56 +117,6 @@ function getSelectedGuitarTuningValue(id: string): Array<number>
 }
 
 
-//////////////////////////////// GUITAR POSITION //////////////////////////////
-
-
-function initGuitarPositionSelector(id: string, useURLParams: boolean = true,
-    nbPos: number = 7, position: number = -1): void
-{
-    // get selecor
-    const posSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById(id);
-    const initialized = (posSelect.options != null && posSelect.options.length > 0);
-    if (initialized) // nop if already initialized
-        return;
-
-    // add tunings
-    for (let i = -1; i < nbPos; i++)
-    {
-        const positionAll = (i == -1);
-
-        let option = document.createElement('option');
-        option.value = i.toString();
-        option.innerHTML = positionAll ? "All" : (i + 1).toString();
-        if (i == position)
-            option.selected = true;
-
-        posSelect.appendChild(option);
-    }
-
-    // disable if only 1 option
-    posSelect.disabled = (posSelect.options.length <= 1);
-}
-
-function updateGuitarPositionGivenNbNotes(id: string, nbNotes: number): void
-{
-    const posSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById(id);
-    const posFormer: number = getSelectedGuitarPosition(id);
-
-    // replace selector options and try to select former corresponding guitar tuning
-    removeAllChildNodes(posSelect);
-    const position = (posFormer <= nbNotes) ? posFormer : -1;
-    initGuitarPositionSelector(id, false, nbNotes, position);
-}
-
-function getSelectedGuitarPosition(id: string): number
-{
-    const posSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById(id);
-    let posId = posSelect.value;
-
-    return parseInt(posId);
-}
-
-
 //////////////////////////////////// UTILS ////////////////////////////////////
 
 
