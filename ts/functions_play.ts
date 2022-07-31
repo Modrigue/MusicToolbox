1a13d1bf0c2d771351092a5e1ad2d780cc2d04ba
Modrigue <thealdobaldo@gmail.com>
2022-07-31 19:06:11 +0200

    WIP: 1st test of scale keyboard play

--- ts/functions_play.ts
+++ ts/functions_play.ts
@@ -199,3 +199,219 @@ function onPlayChordInScale(nbNotesInChords: number, index: number, step: number
     const noteCurrent: number = noteValue + scaleValues[index];
     playChord(noteCurrent, chordValues, duration, delay);   
 }
+
+
+///////////////////////////// KEYBOARD PLAY FUNCTIONS /////////////////////////
+
+
+const noteValueMin = 24;  // C0
+const noteValueMax = 108; // C7
+let notesPressed: Array<number> = [];
+
+document.addEventListener('keydown', function(e)
+{
+  // get tonic value and scale values
+  const tonicValue = getSelectedNoteValue();
+  const scaleValues: Array<number> = getScaleValues();
+  const nbNotesInScale = scaleValues.length;
+
+  const position = getPositionFromInputKey(e, nbNotesInScale);
+  //console.log(`Key down ${e.key} code: ${e.code} => pos: ${position}`);
+
+  if (position < 0)
+    return;
+
+  const positionInScale = position % nbNotesInScale;
+  const octave = Math.floor(position / nbNotesInScale);
+
+  const noteValue = tonicValue + scaleValues[positionInScale] + 12*octave;
+
+  // compute pitch bend if non-integer value
+  let pitchBend = noteValue - Math.floor(noteValue);
+  pitchBend *= 1 / 8 / 2; // 1/8/2 = 1/2 tone
+
+  if(notesPressed.indexOf(noteValue) < 0 && noteValueMin + noteValue <= noteValueMax)
+  {
+      MIDI.noteOn(0, noteValueMin + Math.floor(noteValue), 60, 0, pitchBend);
+      notesPressed.push(noteValue);
+  }
+
+  //console.log(notesPressed);
+}, false);
+
+document.addEventListener('keyup', function(e)
+{
+  // get tonic value and scale values
+  const tonicValue = getSelectedNoteValue();
+  const scaleValues: Array<number> = getScaleValues();
+  const nbNotesInScale = scaleValues.length;
+    
+  const position = getPositionFromInputKey(e, nbNotesInScale);
+  if (position < 0)
+    return;
+
+
+  const positionInScale = position % nbNotesInScale;
+  const octave = Math.floor(position / nbNotesInScale);
+
+  const noteValue = tonicValue + scaleValues[positionInScale] + 12*octave;
+
+  const noteIndex = notesPressed.indexOf(noteValue, 0);
+  if(noteIndex >= 0  && noteValueMin + noteValue <= noteValueMax)
+  {
+    MIDI.noteOff(0, noteValueMin + Math.floor(noteValue));
+    notesPressed.splice(noteIndex, 1);
+  }
+
+  //console.log(notesPressed);
+}, false);
+
+
+function getPositionFromInputKey(e: KeyboardEvent, nbNotesInScale: number): number
+{
+    let position = -999;
+    switch (e.code)
+    {
+        // 1st row
+        case "IntlBackslash":
+            position = 0;
+            break;
+        case "KeyZ":
+            position = 1;
+            break;
+        case "KeyX":
+            position = 2;
+            break;
+        case "KeyC":
+            position = 3;
+            break;
+        case "KeyV":
+            position = 4;
+            break;
+        case "KeyB":
+            position = 5;
+            break;
+        case "KeyN":
+            position = 6;
+            break;
+        case "KeyM":
+            position = 7;
+            break;
+        case "Comma":
+            position = 8;
+            break;
+        case "Period":
+            position = 9;
+            break;
+        case "Slash":
+            position = 10;
+            break;
+
+        // 2nd row
+        case "KeyA":
+            position = 11;
+            break;
+        case "KeyS":
+            position = 12;
+            break;
+        case "KeyD":
+            position = 13;
+            break;
+        case "KeyF":
+            position = 14;
+            break;
+        case "KeyG":
+            position = 15;
+            break;
+        case "KeyH":
+            position = 16;
+            break;
+        case "KeyJ":
+            position = 17;
+            break;
+        case "KeyK":
+            position = 18;
+            break;
+        case "KeyL":
+            position = 19;
+            break;
+        case "Semicolon":
+            position = 20;
+            break;
+        case "Quote":
+            position = 21;
+            break;
+        case "Backslash":
+            position = 22;
+            break;      
+
+        // 3rd row
+        case "KeyQ":
+            position = 23;
+            break;
+        case "KeyW":
+            position = 24;
+            break;
+        case "KeyE":
+            position = 25;
+            break;
+        case "KeyR":
+            position = 26;
+            break;
+        case "KeyT":
+            position = 27;
+            break;
+        case "KeyY":
+            position = 28;
+            break;
+        case "KeyU":
+            position = 29;
+            break;
+        case "KeyI":
+            position = 30;
+            break;
+        case "KeyO":
+            position = 31;
+            break;
+        case "KeyP":
+            position = 32;
+            break;
+        case "BracketLeft":
+            position = 33;
+            break;
+        case "BracketRight":
+            position = 34;
+            break;
+    
+        // 4th row
+        case "Digit1":
+        case "Digit2":
+        case "Digit3":
+        case "Digit4":
+        case "Digit5":
+        case "Digit6":
+        case "Digit7":
+        case "Digit8":
+        case "Digit9":
+            for (let i = 1; i <= 9; i++)
+            {
+                if (e.code == `Digit${i}`)
+                {
+                    position = 34 + i;
+                    break;
+                }
+            }
+            break;
+        case "Digit0":
+            position = 44;
+            break;
+        case "Minus":
+            position = 45;
+            break;
+        case "Equal":
+            position = 46;
+            break;
+    }
+
+    return position;
+}
