2b2ffa68ae99d7072d1250b179df1b4799c0ee71
Modrigue <thealdobaldo@gmail.com>
2023-05-18 17:46:36 +0200

    reorganized files

---
+++ js/tools/roman_numerals.js
@@ -0,0 +1,16 @@
+"use strict";
+// /roman digits
+const romanDigits = new Map();
+romanDigits.set(1, "I");
+romanDigits.set(2, "II");
+romanDigits.set(3, "III");
+romanDigits.set(4, "IV");
+romanDigits.set(5, "V");
+romanDigits.set(6, "VI");
+romanDigits.set(7, "VII");
+romanDigits.set(8, "VIII");
+romanDigits.set(9, "IX");
+romanDigits.set(10, "X");
+romanDigits.set(11, "XI");
+romanDigits.set(12, "XII");
+//# sourceMappingURL=roman_numerals.js.map
